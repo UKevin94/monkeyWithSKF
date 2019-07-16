7347efee88b27806f664955b150c0174b4b4453e
Kévin Ung <kung@NX-DOMAIN.LAN>
2019-07-16 14:59:47 +0200

    First commit

---
+++ src/squashTA/resources/selenium-resources/src/test/java/singe/banana/RandomTestJUnit4.java
@@ -0,0 +1,48 @@
+package user.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class RandomTestJUnit4 {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Before
+    public void setUp(){
+        try{
+            driver = new ChromeDriver();
+            driver.get("https://www.google.com");
+            
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver,30);
+    }
+
+    @Test
+    public void randomTest(){
+    	driver.findElement(By.xpath("//input[@title='Rechercher']")).sendKeys("selenium junit 4");
+    	//wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[@title='Rechercher']"))));
+        Assert.assertTrue(true);
+    }
+
+    @After
+    public void tearDown(){
+        //driver.quit();
+    }
+
+}
---
+++ src/squashTA/resources/selenium-resources/src/test/java/singe/banana/RandomTestJUnit5.java
@@ -0,0 +1,52 @@
+package user.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class RandomTestJUnit5 {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    static final String LOGIN = "skf";
+    static final String FIRST_NAME = "skf";
+    static final String LAST_NAME = "skf";
+    static final String EMAIL = "skf@skf.skf";
+
+    @BeforeEach
+    public void setUp(){
+        try{
+            driver = new ChromeDriver();
+            driver.get("https://www.google.com");
+            
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver,30);
+    }
+
+    @Test
+    public void randomTest(){
+    	driver.findElement(By.xpath("//input[@title='Rechercher']")).sendKeys("selenium junit 5");
+    	//wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[@title='Rechercher']"))));
+        Assertions.assertTrue(true, "");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        //driver.quit();
+    }
+
+}
